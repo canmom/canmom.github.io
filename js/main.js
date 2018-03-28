ef8cce293a65fb96170364f7d77cd8fe0b916de1
canmom <bryn.dickinson@gmail.com>
2018-03-28 21:51:41 +0100

    Add click-to-reveal element
    Articles in Markdown can now include the option to have a hidden block
    with a button that can be clicked to reveal and hide it. This is useful
    for long 'optional' parts of a page, or hiding solutions to problems.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+const reveal = function(id) {
+  document.getElementById(id).style.display = 'block'
+  document.getElementById(id+'button').innerHTML = 'hide'
+  document.getElementById(id+'button').onclick = function() { hide(id) }
+}
 
+const hide = function(id) {
+  document.getElementById(id).style.display = 'none'
+  document.getElementById(id+'button').innerHTML = 'reveal'
+  document.getElementById(id+'button').onclick = function() { reveal(id) }
+}
