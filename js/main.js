7eb9bbfa2ce2be3b198da71d667bfb545904fa8e
canmom <bryn.dickinson@gmail.com>
2018-04-08 19:49:25 +0100

    Implement 'choice box' include for quiz questions
    This allows an unordered list to be transformed into a list of clickable options, with the ability to specify a correct answer. When an option is clicked, it will either be given a tick or a cross and the other answers greyed out; in the latter case the correct answer will be highlighted and marked with an arrow.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,78 @@
-const reveal = function(id) {
+const reveal = function (id) {
   document.getElementById(id).style.display = 'block'
-  document.getElementById(id+'button').innerHTML = 'hide'
-  document.getElementById(id+'button').onclick = function() { hide(id) }
+  document.getElementById(id + 'button').innerHTML = 'hide'
+  document.getElementById(id + 'button').onclick = function () {
+    hide(id)
+  }
 }
 
-const hide = function(id) {
+const hide = function (id) {
   document.getElementById(id).style.display = 'none'
-  document.getElementById(id+'button').innerHTML = 'reveal'
-  document.getElementById(id+'button').onclick = function() { reveal(id) }
+  document.getElementById(id + 'button').innerHTML = 'reveal'
+  document.getElementById(id + 'button').onclick = function () {
+    reveal(id)
+  }
+}
+
+class Answers {
+  constructor () {
+    this.correct = {}
+  }
+
+  register (id, correctAnswer) {
+    this.correct.id = correctAnswer
+  }
+
+  check (id, answer) {
+    return answer === this.correct.id
+  }
+
+  getCorrectAnswer (id) {
+    return correct.id
+  }
+}
+
+const pageAnswers = new Answers()
+
+const makeUnselectable = function (optionElement) {
+  optionElement.classList.remove('selectable')
+  optionElement.onclick = null
+}
+
+const resolveChoice = function (id, answer, optionsList) {
+  if (pageAnswers.check(id, answer)) {
+    optionsList.forEach(function (currentOption, currentIndex) {
+      makeUnselectable(currentOption)
+
+      currentOption.classList.add(
+        currentIndex === answer ? 'correct' : 'greyedout'
+      )
+    })
+  } else {
+    optionsList.forEach(function (currentOption, currentIndex) {
+      makeUnselectable(currentOption)
+
+      let status
+      if (currentIndex === answer) {
+        status = 'wrong'
+      } else if (pageAnswers.check(id, currentIndex)) {
+        status = 'correction'
+      } else {
+        status = 'greyedout'
+      }
+      currentOption.classList.add(status)
+    })
+  }
+}
+
+const setUpChoices = function (id, correctAnswer) {
+  pageAnswers.register(id, correctAnswer)
+  const optionsList = document.querySelectorAll('#' + id + ' ul li')
+
+  optionsList.forEach(function (currentOption, currentIndex) {
+    currentOption.classList.add('selectable')
+    currentOption.onclick = function () {
+      resolveChoice(id, currentIndex, optionsList)
+    }
+  })
 }
